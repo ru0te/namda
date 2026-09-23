beb83c3583d1b33466518b0cabec938677bf2d86
ru0te <eniadew69@gmail.com>
2026-09-23 16:19:53 +0100

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { Routes, Route } from "react-router";
+import Layout from "./components/layout/Layout";
+import HomePage from "./pages/HomePage";
+import BrowsePage from "./pages/BrowsePage";
+import TitlePage from "./pages/TitlePage";
+
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/browse" element={<BrowsePage />}></Route>
+        <Route path="/title/:id" element={<TitlePage />}></Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/BrowsePage.tsx
@@ -0,0 +1,5 @@
+function BrowsePage() {
+  return <></>;
+}
+
+export default BrowsePage;
